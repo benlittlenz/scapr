506ae219ed8c6a50eddfc31eed4de4b5f1e7e4e7
nzbenji <benlittle53@gmail.com>
2019-10-10 06:52:27 +0100

    added express config

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('listening on port'))
