16bbb23fb13d6262b4a39deb153d16c85952640a
nzbenji <benlittle53@gmail.com>
2019-10-12 03:43:02 +0100

    Added frontend

---
+++ frontend/.next/static/webpack/static/development/pages/index.js.7b812b333aac42079a26.hot-update.js
@@ -0,0 +1,31 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Home; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/ben/scrapr/frontend/pages/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+function Home() {
+  return __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "Home page");
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.7b812b333aac42079a26.hot-update.js.map
